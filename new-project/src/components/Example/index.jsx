19587359064dad3fa1fde5043d79218e8a1ee74b
VladSmaug <incrediblesmaug@gmail.com>
2022-07-29 15:34:57 +0300

    With working button

--- new-project/src/components/Example/index.jsx
+++ new-project/src/components/Example/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { styles } from "";
 
 const Example = () => {
   const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className={styles.example}>
+      <h1>Counter App</h1>
       <p>Ви натиснули {count} разів</p>
       <button onClick={() => setCount(count + 1)}>Натисни мене</button>
     </div>
