80fb152f217d1bd2bbc17fe68bcd50f964a3dc69
VladSmaug <incrediblesmaug@gmail.com>
2022-07-29 17:54:00 +0300

    With corrections

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -2,12 +2,12 @@ import Example from "./components/Example";
 import styles from "./App.module.css";
 import AnotherExample from "./components/AnotherExample";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Example />
       <AnotherExample />;
     </div>
   );
-}
+};
 export default App;
