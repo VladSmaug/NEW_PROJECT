2337ddad837d9dbb63350c471eba67f7887f2233
VladSmaug <incrediblesmaug@gmail.com>
2022-07-29 17:50:41 +0300

    Arrow function changing declaration

--- new-project/src/components/AnotherExample/index.jsx
+++ new-project/src/components/AnotherExample/index.jsx
@@ -3,12 +3,12 @@ import styles from "./index.module.css";
 
 const AnotherExample = () => {
   const [counter, setCounter] = useState(0);
-  function increment() {
+  const increment = () => {
     setCounter(counter + 1);
-  }
-  function decrement() {
+  };
+  const decrement = () => {
     setCounter(counter - 1);
-  }
+  };
   return (
     <div className={styles.example}>
       <h2>Counter {counter}</h2>
