6f7335a36961b949594ef3a6c7d4fb7120ebbb74
VladSmaug <incrediblesmaug@gmail.com>
2022-08-11 16:16:07 +0300

    Starting

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,29 +1,10 @@
-import Counter from "./components/Counter";
-import AnotherCounter from "./components/AnotherCounter";
-import ArrayChanger from "./components/Array";
-import Comp from "./components/UserInputFormExample";
-import FormWithRef from "./components/UserInputFormExampleRef";
-import User from "./components/SandboxComponent";
-
 import styles from "./App.module.css";
-import { useState } from "react";
 
 const App = () => {
-  const [visibleUser, setVisibleUser] = useState(true);
-  const toogleVisibleUser = () => {
-    setVisibleUser((visible) => !visible);
-  };
   return (
-    <div className={styles.wrapper}>
-      <Counter />
-      <AnotherCounter />;
-      <ArrayChanger />
-      <Comp />
-      <FormWithRef />
-      {visibleUser && <User />}
-      <button className={styles.showhide} onClick={toogleVisibleUser}>
-        Show / Hide
-      </button>
+    <div className={styles.App}>
+      <h1>React app</h1>
+      <div></div>
     </div>
   );
 };
