cdced30c066845865191e6981e8fbca33e215b3b
VladSmaug <incrediblesmaug@gmail.com>
2022-08-12 15:35:08 +0300

    With new style

--- new-project/src/Components/Todo.js
+++ new-project/src/Components/Todo.js
@@ -16,7 +16,9 @@ const Todo = ({ text, setTodos, todos, todo }) => {
   };
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
       <button onClick={completeHanler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
